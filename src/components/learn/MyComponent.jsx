b39ea8a93fbdf4b693338a45453c94f110499ff9
BaoPham1998 <baopham199807@gmail.com>
2024-11-10 01:10:37 +0900

    video30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,5 +1,15 @@
+
+// <> fragment có chức năng không phá vỡ cấu trúc của css
+
+import './style.css'
+
 const MyComponent = () => {
-    return (<div>BAO PHAM & ERIC</div>)
+    return (
+        <>
+            <div>BAO PHAM & ERIC</div>
+            <div className='child'>hahahaha</div>
+        </>
+    );
 }
 
 export default MyComponent;
