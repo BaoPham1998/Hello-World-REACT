6f6b9651d316ff7f8e98c5604644f5d83820cdc6
BaoPham1998 <baopham199807@gmail.com>
2024-11-12 00:53:06 +0900

    video36

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,12 +1,21 @@
 const TodoNew = (props) => {
     console.log(props);
     const { addTodo } = props;
-    addTodo("bao Pham");
+    // addTodo("bao Pham");
 
+    const handleClick = () => {
+        alert("Click me")
+    }
+    const handleOnChange = (name) => {
+        console.log(">>> handleOnChange", name);
+    }
     return (
         <div className="todo-new">
-            <input type="text" className="todo-input" placeholder="Enter your task" />
-            <button className="todo-button">Add</button>
+            <input type="text" className="todo-input" placeholder="Enter your task"
+                onChange={(event) => { handleOnChange(event.target.value) }}
+            />
+            <button className="todo-button"
+                onClick={handleClick}>Add</button>
         </div>
     )
 }
